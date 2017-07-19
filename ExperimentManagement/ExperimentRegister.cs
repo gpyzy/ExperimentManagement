a8540f381989a5a92d52e177237e74d2408187b8
Jony Yang <jony.yang@agoda.com>
2017-07-19 18:09:40 +0700

    Complete some demo cases.

--- ExperimentManagement/ExperimentRegister.cs
+++ ExperimentManagement/ExperimentRegister.cs
@@ -6,158 +6,160 @@ using System.Threading;
 
 namespace ExperimentManagement
 {
-	public class ExperimentRegister
-	{
+    public class ExperimentRegister
+    {
 
-		private bool stopWatchStats = false;
-		private bool callStackStats = false;
-		private ILog logger = new Logger();
-		private IExperimentManager expMgr = new ExperimentManager();
+        private bool stopWatchStats = false;
+        private bool callStackStats = false;
+        private ILog logger = new Logger();
+        private IExperimentManager expMgr = new ExperimentManager();
 
-		private Dictionary<string, ExperimentMetadata> experimentList = new Dictionary<string, ExperimentMetadata>();
-		private ConcurrentDictionary<int, SortedList<string, int>> experimentStatistics = new ConcurrentDictionary<int, Dictionary<string, int>>();
+        private Dictionary<string, ExperimentMetadata> experimentList = new Dictionary<string, ExperimentMetadata>();
+        private ConcurrentDictionary<int, SortedList<string, int>> experimentStatistics = new ConcurrentDictionary<int, SortedList<string, int>>();
 
-		private ExperimentRegister()
-		{
-			//System.Threading.Thread
-		}
+        private ExperimentRegister()
+        {
+            //System.Threading.Thread
+        }
 
-		#region properties
-		private static ExperimentRegister Instance
-		{
-			get
-			{
-				return InternalClass.instance;
-			}
-		}
+        #region properties
+        private static ExperimentRegister Instance
+        {
+            get
+            {
+                return InternalClass.instance;
+            }
+        }
 
-		#endregion
+        #endregion
 
-		#region Methods
-		public static ExperimentRegister Register(string experimentId, string experimentName = "")
-		{
-			Instance.experimentList.Add(experimentId, ExperimentMetadata.New(experimentId, experimentName));
-			return Instance;
-		}
-		public static ExperimentRegister Register(IList<string> experimentNames)
-		{
-			foreach (var name in experimentNames)
-			{
-				Register(name);
-			}
+        #region Methods
+        public static ExperimentRegister Register(string experimentId, string experimentName = "")
+        {
+            Instance.experimentList.Add(experimentId, ExperimentMetadata.New(experimentId, experimentName));
+            return Instance;
+        }
+        public static ExperimentRegister Register(IList<string> experimentNames)
+        {
+            foreach (var name in experimentNames)
+            {
+                Register(name);
+            }
 
-			return Instance;
-		}
+            return Instance;
+        }
 
-		public static ExperimentRegister WithStopWatchStats(bool stopWatchStats)
-		{
-			Instance.stopWatchStats = stopWatchStats;
-			return Instance;
-		}
+        public static ExperimentRegister WithStopWatchStats(bool stopWatchStats)
+        {
+            Instance.stopWatchStats = stopWatchStats;
+            return Instance;
+        }
 
-		public static ExperimentRegister WithCallStackStats(bool callStackStats)
-		{
-			Instance.callStackStats = callStackStats;
-			return Instance;
-		}
+        public static ExperimentRegister WithCallStackStats(bool callStackStats)
+        {
+            Instance.callStackStats = callStackStats;
+            return Instance;
+        }
 
-		public static ExperimentRegister WithLog(ILog logger)
-		{
-			Instance.logger = logger;
-			return Instance;
-		}
+        public static ExperimentRegister WithLog(ILog logger)
+        {
+            Instance.logger = logger;
+            return Instance;
+        }
 
-		public static void Experiment(string experimentId, Action<char> experimentAction)
-		{
-			if (Instance.experimentList.ContainsKey(experimentId))
-			{
-				var peekResult = Instance.expMgr.Peek(experimentId);
+        public static void Experiment(string experimentId, Action<char> experimentAction)
+        {
+            if (Instance.experimentList.ContainsKey(experimentId))
+            {
+                var peekResult = Instance.expMgr.Peek(experimentId);
+                Action actionToRun = () => { experimentAction(peekResult); };
+                if (Instance.stopWatchStats)
+                {
+                    actionToRun = ExperimentWithStopWatchStats(actionToRun);
+                }
 
-				var actionToRun = experimentAction;
+                if (Instance.callStackStats)
+                {
+                    actionToRun = ExperimentWithCallStackStats(actionToRun, experimentId);
+                }
 
-				if (Instance.stopWatchStats)
-				{
-					actionToRun = (char pr) =>
-					{
-						/// TODO - use the elapsedTime for performance statistics
-						var elapsedTime = ExperimentWithStopWatch(actionToRun, pr);
-					};
-				}
-				if (Instance.callStackStats)
-				{
-					actionToRun = (char pr) =>
-					{
-						ExperimentWithCallStackStats(actionToRun, pr, experimentId);
+                actionToRun();
+            }
+            else
+            {
+                Instance.logger.Warn("The experiment '{0}' is not regiestered", experimentId);
+            }
 
-					};
-				}
+            
+        }
 
-			}
-			else
-			{
-				Instance.logger.Warn("The experiment '{0}' is not regiestered", experimentId);
-			}
-
-		}
-
-		private static long ExperimentWithStopWatch(Action<char> experimentAction, char input)
-		{
-			Stopwatch sw = new Stopwatch();
-			sw.Start();
-			experimentAction(input);
-			sw.Stop();
-			return sw.ElapsedMilliseconds;
-		}
+        private static Action ExperimentWithStopWatchStats(Action experimentAction)
+        {
+            return () =>
+            {
+                
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                experimentAction();
+                sw.Stop();
+                //return sw.ElapsedMilliseconds;
+                Instance.logger.Debug("ExperimentWithStopWatchStats called.");
+            };
+        }
 
 
-		private static void ExperimentWithCallStackStats(Action<char> experimentAction, char input, string experimentId)
-		{
-			experimentAction(input);
+        private static Action ExperimentWithCallStackStats(Action experimentAction, string experimentId)
+        {
+            return () =>
+            {
+                experimentAction();
 
-			try
-			{
-				var threadId = Thread.CurrentThread.ManagedThreadId;
-				var sortList = PrepareExperimentStatistics(threadId);
+                /// TODO - performance enhancements
+                try
+                {
+                    var threadId = Thread.CurrentThread.ManagedThreadId;
+                    var sortList = PrepareExperimentStatistics(threadId);
 
-				if (sortList.ContainsKey(experimentId))
-				{
-					sortList[experimentId]++;
-				}
-				else
-				{
-					sortList.Add(experimentId, 1);
-				}
-			}
-			catch (Exception ex)
-			{
-				Instance.logger.Error("ExperimentWithCallStackStats faliure: " + ex.Message);
-			}
+                    if (sortList.ContainsKey(experimentId))
+                    {
+                        sortList[experimentId]++;
+                    }
+                    else
+                    {
+                        sortList.Add(experimentId, 1);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Instance.logger.Error("ExperimentWithCallStackStats faliure: " + ex.Message);
+                }
+
+                Instance.logger.Debug("ExperimentWithCallStackStats called.");
+            };
+
+        }
+
+        private static SortedList<string, int> PrepareExperimentStatistics(int key)
+        {
+            if (!Instance.experimentStatistics.ContainsKey(key))
+            {
+                if (!Instance.experimentStatistics.TryAdd(key, new SortedList<string, int>()))
+                {
+                    /// https://stackoverflow.com/questions/11501931/can-concurrentdictionary-tryadd-fail
+                    /// Nothing to do here
+                }
+            }
+
+            return Instance.experimentStatistics[key];
+        }
 
 
-
-		}
-
-		private static SortedList<string, int> PrepareExperimentStatistics(int key)
-		{
-			if (!Instance.experimentStatistics.ContainsKey(key))
-			{
-				if (!Instance.experimentStatistics.TryAdd(key, new SortedList<string, int>()))
-				{
-					/// https://stackoverflow.com/questions/11501931/can-concurrentdictionary-tryadd-fail
-					/// Nothing to do here
-				}
-			}
-
-			return Instance.experimentStatistics[key];
-		}
+        #endregion
 
 
-		#endregion
-
-
-		private static class InternalClass
-		{
-			public static ExperimentRegister instance = new ExperimentRegister();
-		}
-	}
+        private static class InternalClass
+        {
+            public static ExperimentRegister instance = new ExperimentRegister();
+        }
+    }
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,54 +5,90 @@ using ExperimentManagement;
 
 namespace HelloWorld
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			ExperimentRegister.Register(new List<string> { "Exp01", "Exp02", "Exp03" });
-			ExperimentRegister.WithStopWatchStats(true);
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ExperimentRegister.Register(new List<string> { "Exp01", "Exp02", "Exp03" });
+            ExperimentRegister.WithStopWatchStats(true);
+            ExperimentRegister.WithCallStackStats(true);
 
-			for (int i = 0; i < 10; i++)
-			{
-				var rand = new Random().Next(0, 1000);
+            /// DemoExperiment
+            //DemoExperiment();
+            /// DemoEmbedExperiment
+            DemoEmbedExperiment();
+            Console.ReadKey();
+        }
 
-				Thread.Sleep(rand);
-				ExperimentRegister.Experiment("Exp01", (peekResult) =>
-				{
-					switch (peekResult)
-					{
-						case 'A':
-							Helloworld1();
-							return;
-						case 'B':
-							Helloworld2();
-							return;
-						case 'C':
-							Helloworld3();
-							return;
-						default:
-							return;
-					}
-				});
-			}
 
-			Console.ReadKey();
-		}
+        /// <summary>
+        /// DemoExperiment demos how to use ExperimentRegister.Experiment. 
+        /// </summary>
+        public static void DemoExperiment()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var rand = new Random().Next(0, 1000);
 
-		public static void Helloworld1()
-		{
-			Console.WriteLine("Hello World 1!");
+                Thread.Sleep(rand);
+                ExperimentRegister.Experiment("Exp01", (peekResult) =>
+                {
+                    switch (peekResult)
+                    {
+                        case 'A':
+                            Helloworld1();
+                            return;
+                        case 'B':
+                            Helloworld2();
+                            return;
+                        case 'C':
+                            Helloworld3();
+                            return;
+                        default:
+                            return;
+                    }
+                });
+            }
+        }
 
-		}
-		public static void Helloworld2()
-		{
-			Console.WriteLine("Hello World 2!");
+        public static void DemoEmbedExperiment()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                var rand = new Random().Next(0, 1000);
+                Thread.Sleep(rand);
 
-		}
-		public static void Helloworld3()
-		{
-			Console.WriteLine("Hello World 3!");
+                ExperimentRegister.Experiment("Exp01", (exp01) =>
+            {
+                if (exp01 == 'A')
+                {
+                    ExperimentRegister.Experiment("Exp02", (exp02) =>
+                    {
+                        Console.WriteLine("Exp01=A,Exp02=" + exp02);
+                    });
+                }
+                else
+                {
+                    Console.WriteLine("Exp01=" + exp01);
+                }
+            });
 
-		}
-	}
+            }
+        }
+
+        static void Helloworld1()
+        {
+            Console.WriteLine("Hello World 1!");
+
+        }
+        static void Helloworld2()
+        {
+            Console.WriteLine("Hello World 2!");
+
+        }
+        static void Helloworld3()
+        {
+            Console.WriteLine("Hello World 3!");
+        }
+    }
 }
